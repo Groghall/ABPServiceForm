dc85267902683a80785e19a8e02b98bb73e9393c
Stephen Gault <stephengault626@googlemail.com>
2016-04-14 12:07:34 +0100

    ABPServiceForm
    Started Development of ABP Service Form for Fleetsolve Ltd

---
+++ js/abpjs.js
@@ -0,0 +1,9 @@
+$(function () {
+        $("input[name='haz2']").click(function () {
+            if ($("#haz2yes").is(":checked")) {
+                $("#haz2number").show();
+            } else {
+                $("#haz2number").hide();
+            }
+        });
+    });
